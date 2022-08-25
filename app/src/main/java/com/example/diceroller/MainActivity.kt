44013907923d9eaaa6d27aa9d3836ef192aa9a44
e.astafev00@mail.ru <e.astafev00@mail.ru>
2022-08-25 22:03:28 +0300

    before creating a rollDice method 2

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        //val myDice: Dice(10)
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.textView)
+            //resultTextView.text = myDice.roll()
+            //resultTextView.text = "6"
+            //val toast = Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+class Dice (private val numSides: Int){
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
