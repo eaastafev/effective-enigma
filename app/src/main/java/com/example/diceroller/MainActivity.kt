d72695d2b406dcbd9e5195440c928adadb8d6bb5
e.astafev00@mail.ru <e.astafev00@mail.ru>
2022-08-27 11:01:18 +0300

    finished rollDice method (removed unused myDice and toast variables)

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,13 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        //val myDice: Dice(10)
+        //val myDice = Dice(10)
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            //resultTextView.text = myDice.roll()
+            rollDice()
+            //val resultTextView: TextView = findViewById(R.id.textView)
+            //resultTextView.text = myDice.roll().toString()
             //resultTextView.text = "6"
             //val toast = Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+        Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
     }
 }
 
