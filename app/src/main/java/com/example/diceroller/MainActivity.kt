7beefa093e5a217d3118597557b9460bb4d1313a
e.astafev00@mail.ru <e.astafev00@mail.ru>
2022-09-01 19:04:43 +0300

    now there are pictures of dice instead of a number. Code needs optimising and cleaning

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * сделано по мануалу от Гугл
+ * Modify the MainActivity.kt class to add interactive behavior to the app.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -13,11 +18,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         //val myDice = Dice(10)
+        //Set an on-click listener for a Button using setOnClickListener() to add behavior for when a Button is clicked
         rollButton.setOnClickListener {
             rollDice()
             //val resultTextView: TextView = findViewById(R.id.textView)
             //resultTextView.text = myDice.roll().toString()
             //resultTextView.text = "6"
+            //Pop up a Toast message as a temporary solution to verify you're on the right track.
             //val toast = Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
         }
     }
@@ -25,13 +32,23 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceRoll){
+            1-> diceImage.setImageResource(R.drawable.dice_1)
+            2-> diceImage.setImageResource(R.drawable.dice_2)
+            3-> diceImage.setImageResource(R.drawable.dice_3)
+            4-> diceImage.setImageResource(R.drawable.dice_4)
+            5-> diceImage.setImageResource(R.drawable.dice_5)
+            6-> diceImage.setImageResource(R.drawable.dice_6)
+        }
+        //diceImage.setImageResource(R.drawable.dice_2)
+        //val resultTextView: TextView = findViewById(R.id.textView)
+        //resultTextView.text = diceRoll.toString()
         Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
     }
 }
 
-class Dice (private val numSides: Int){
+class Dice(private val numSides: Int) {
 
     fun roll(): Int {
         return (1..numSides).random()
